3ac46ad46687416525c99a38bf69b6be9b6dd00a
Eduardo Raz Guzman <eduardo.raz_guzman@uconn.edu>
2026-03-16 01:31:51 -0400

    feat: added steer function

--- tree/main.cpp
+++ tree/main.cpp
@@ -17,7 +17,7 @@ RRT::Coord sample()
     return {static_cast<double>(x(mt)), static_cast<double>(y(mt))};
 }
 
-double euclidianDistance(const RRT::Coord& p, RRT::Coord& q)
+double euclidianDistance(const RRT::Coord& p, const RRT::Coord& q)
 {
     return sqrt((p.x - q.x) * (p.x - q.x) + (p.y - q.y) * (p.y - q.y));
 }
@@ -38,11 +38,32 @@ RRT::Node* nearest(RRT tree, RRT::Node *newNode)
     return  near;
 }
 
+RRT::Coord steer(const RRT::Coord& target, const RRT::Coord& start)
+{
+    double dx { target.x - start.x };
+    double dy { target.y - start.y };
+
+    double length { sqrt(dx * dx + dy * dy) };
+
+    double stepDx { StepSize * (dx/length) };
+    double stepDy { StepSize * (dy/length) };
+
+    std::cout << "STEPDX: " << stepDx << std::endl;
+    std::cout << "STEPDY: " << stepDy << std::endl;
+
+    return {start.x + stepDx, start.y + stepDy };
+}
+
+
+
 int main()
 {
 
     RRT rrt({0.0,0.0});
     rrt.addNode({1.0,2.0}, rrt.nodes()[0]);
+
+    steer({500, 500}, {0,0});
+
 
     std::cout << "Hello, World!\n";
     return 0;
